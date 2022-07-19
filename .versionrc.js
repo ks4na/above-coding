088604bcab62cd123b08208890fc8f8de6544871
qiuwei <qiuwei@fullgoalos.com.cn>
2022-07-19 17:43:49 +0800

    chore: add standard-version

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: ':sparkles: Features' },
+    { type: 'fix', section: ':bug: Bug Fixes' },
+    { type: 'chore', section: ':wrench: Chore' },
+    { type: 'docs', hidden: true },
+    { type: 'style', hidden: true },
+    { type: 'refactor', hidden: true },
+    { type: 'perf', hidden: true },
+    { type: 'test', hidden: true },
+  ],
+}
